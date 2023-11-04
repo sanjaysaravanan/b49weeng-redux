e3a2482b5573247bd14e911c2724e88339fafda5
Sanjay Saravanan <sanjaysaravanan38@gmail.com>
2023-11-04 11:38:38 +0530

    added localStorage for redux

--- src/store/store.js
+++ src/store/store.js
@@ -3,12 +3,30 @@ import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./reducers/counterReducer";
 import todoReducer from "./reducers/todoReducer";
 
+const storeToLocalStorage = (props) => {
+  return (next) => (action) => {
+    const result = next(action); // doing the action & getting the result
+    localStorage.setItem("redux_state", JSON.stringify(props.getState())); // loading the resux state into localStorage
+    return result;
+  };
+};
+
 const store = configureStore({
   reducer: {
     counterReducer,
     todoReducer,
   },
-  devTools: true, // enable dev tools extension should be false for prod
+  devTools: true, // enable dev tools extension should be false for production
+  preloadedState: (function () {
+    if (localStorage.getItem("redux_state") !== null) {
+      // if storage values found, then load the values when we refresh
+      return JSON.parse(localStorage.getItem("redux_state"));
+    }
+  })(),
+  middleware: (defaultMiddleWares) => [
+    ...defaultMiddleWares(),
+    storeToLocalStorage,
+  ],
 });
 
 export default store;
